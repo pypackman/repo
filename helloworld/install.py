d8dc74047c69ef9a733ff694f33089eaa6981283
ezintegg2398 <easontekreviews@icloud.com>
2022-06-22 16:31:55 +0800

    update helloworld

--- helloworld/install.py
+++ helloworld/install.py
@@ -1,8 +1,11 @@
-import os, stat, requests
+import os, stat, wget
 
-package = requests.get('https://raw.githubusercontent.com/repo/helloworld/dist/helloworld')
-with open('/tmp/package', 'wb') as pack:
-    pack.write(package.content)
-    os.mkdir(os.path.expanduser("~/.bin/pypack/helloworld"))
+def install():
+    wget.download('https://raw.githubusercontent.com/repo/helloworld/dist/helloworld', '/tmp/')
+    if not os.path.isdir('~/.bin/pypack'):
+        print("PyPack has not been installed Correctly! please install it with ensure_pypack.")
+        pass
+    else:
+        os.mkdir(os.path.expanduser("~/.bin/pypack/helloworld"))
     os.rename("/tmp/package", os.path.expanduser("~/.bin/pypack/helloworld/helloworld"))
-os.chmod(os.path.expanduser("~/.bin/pypack/helloworld/helloworld"), os.stat(os.path.expanduser("~/.bin/pypack/helloworld/helloworld")).st_mode | stat.S_IEXEC)
+    os.chmod(os.path.expanduser("~/.bin/pypack/helloworld/helloworld"), os.stat(os.path.expanduser("~/.bin/pypack/helloworld/helloworld")).st_mode | stat.S_IEXEC)
