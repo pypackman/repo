20197754924cc99f0873a08fead8adedc49a284b
ezintegg2398 <easontekreviews@icloud.com>
2022-06-22 16:54:36 +0800

    update install

--- helloworld/install.py
+++ helloworld/install.py
@@ -10,10 +10,10 @@ def install():
             os.remove('/tmp/install.py')
         except:
             exit()
-    if not os.path.isdir('~/.bin/pypack'):
+    if os.path.isdir('~/.bin/pypack'):
+        os.mkdir(os.path.expanduser("~/.bin/pypack/helloworld"))
+    else:
         print("PyPack has not been installed Correctly! please install it with ensure_pypack.")
         exit()
-    else:
-        os.mkdir(os.path.expanduser("~/.bin/pypack/helloworld"))
     os.rename("/tmp/package", os.path.expanduser("~/.bin/pypack/helloworld/helloworld"))
     os.chmod(os.path.expanduser("~/.bin/pypack/helloworld/helloworld"), os.stat(os.path.expanduser("~/.bin/pypack/helloworld/helloworld")).st_mode | stat.S_IEXEC)
