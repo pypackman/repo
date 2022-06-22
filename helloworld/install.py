2ab77909f9a1c89bbb6f6c250d631a5576728e51
ezintegg2398 <easontekreviews@icloud.com>
2022-06-22 08:26:19 +0800

    changes

--- helloworld/install.py
+++ helloworld/install.py
@@ -1,4 +1,8 @@
-import os, stat
+import os, stat, requests
 
-os.rename("dist", os.path.expanduser("~/.bin/pypack/helloworld"))
+package = requests.get('https://raw.githubusercontent.com/repo/helloworld/dist/helloworld')
+with open('/tmp/package', 'wb') as pack:
+    pack.write(package.content)
+    os.mkdir(os.path.expanduser("~/.bin/pypack/helloworld"))
+    os.rename("/tmp/package", os.path.expanduser("~/.bin/pypack/helloworld/helloworld"))
 os.chmod(os.path.expanduser("~/.bin/pypack/helloworld/helloworld"), os.stat(os.path.expanduser("~/.bin/pypack/helloworld/helloworld")).st_mode | stat.S_IEXEC)
